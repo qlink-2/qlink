6bcfa11e6aa6232ca70980c9ad4a3939d91e84a0
Mayank <a7x.mayank@gmail.com>
2021-03-09 20:53:03 -0500

    Add basic test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,23 +1,14 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:qlink/main.dart';
+import 'package:qlink/main.dart';
 
 void main() {
-  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-  //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(QlinkApp());
+  testWidgets('Send screen test', (WidgetTester tester) async {
+    await tester.pumpWidget(QlinkApp());
 
-  //   // Verify that our counter starts at 0.
-  //   expect(find.text('0'), findsOneWidget);
-  //   expect(find.text('1'), findsNothing);
-
-  //   // Tap the '+' icon and trigger a frame.
-  //   await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pump();
-
-  //   // Verify that our counter has incremented.
-  //   expect(find.text('0'), findsNothing);
-  //   expect(find.text('1'), findsOneWidget);
-  // });
+    expect(find.byType(TextField), findsOneWidget);
+    expect(find.widgetWithText(ElevatedButton, 'Send as QR'), findsOneWidget);
+    expect(find.widgetWithText(OutlinedButton, 'Upload a file'), findsOneWidget);
+  });
 }
